58e3a928efa7eeb853cd348f2cd82f6404a84963
Spencer Florence <florence@ccs.neu.edu>
2015-07-15 15:46:20 -0500

    cleaning up tests

--- pict-test/tests/pict/main.rkt
+++ pict-test/tests/pict/main.rkt
@@ -20,23 +20,23 @@
   (equal? (->bitmap actual) (->bitmap expected)))
 
 
+
 (test-case
  "freeze random testing"
  (define (generate [depth 0])
    (define (gen) (generate (add1 depth)))
-   (define elems
-     (vector (lambda () (text "sefsefse"))
-             (lambda () (rectangle (random 100) (random 100)))
-             (lambda () (arrow (random 100) (random 100)))
-             (lambda () (frame (gen)))
-             (lambda () (cc-superimpose (gen) (gen)))
-             (lambda () (vl-append (gen) (gen)))
-             (lambda () (hbl-append (gen) (gen)))
-             (lambda () (rb-superimpose (gen) (gen)))
-             (lambda () (panorama (gen)))
-             (lambda () (scale (gen) (random)))
-             (lambda () (inset (gen) (random 10) (random 10) (random 10) (random 10)))))
-   ((vector-ref elems (if (> depth 4) (random 3) (random 10)))))
+   (case (if (> depth 4) (random 3) (random 11))
+     [(0) (text "sefsefse")]
+     [(1) (rectangle (random 100) (random 100))]
+     [(2) (arrow (random 100) (random 100))]
+     [(3) (frame (gen))]
+     [(4) (cc-superimpose (gen) (gen))]
+     [(5) (vl-append (gen) (gen))]
+     [(6) (hbl-append (gen) (gen))]
+     [(7) (rb-superimpose (gen) (gen))]
+     [(8) (panorama (gen))]
+     [(9) (scale (gen) (random))]
+     [(10) (inset (gen) (random 10) (random 10) (random 10) (random 10))]))
  (for ([i 1000])
    (define p (generate))
    (check-pict=? p (freeze p))))
